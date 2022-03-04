46e423197273cf0024b65ef1d328111b424eec30
ChrisSchreiber <ChrisSchreiber@users.noreply.github.com>
2022-03-04 22:53:01 +0000

    ci: apply prettier changes

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 test("renders GitHub is awesome text", () => {
   render(<App />);
   const linkElement = screen.getByText(/GitHub is awesome/i);
-  const footer = screen.getByText(/Automate All The Things!!!/i)
+  const footer = screen.getByText(/Automate All The Things!!!/i);
   expect(linkElement).toBeInTheDocument();
   expect(footer).toBeInTheDocument();
 });
