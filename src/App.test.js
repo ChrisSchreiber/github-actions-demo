411886b8ef27ae656da78cab3cd59501452dbd85
ChrisSchreiber <ChrisSchreiber@users.noreply.github.com>
2022-03-04 05:17:00 +0000

    ci: apply prettier changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders GitHub is awesome text', () => {
+test("renders GitHub is awesome text", () => {
   render(<App />);
   const linkElement = screen.getByText(/GitHub is awesome/i);
   expect(linkElement).toBeInTheDocument();
